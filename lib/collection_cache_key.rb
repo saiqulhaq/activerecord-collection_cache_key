aaf2c169af1e581e130ff2434c1831b574231d6e
Dan Drinkard <dan.drinkard@customink.com>
2016-03-21 15:07:42 -0400

    Bring the contract inline with Rails 5 and de-namespace the tests to cover the spread between Base and Relation impls

--- lib/collection_cache_key.rb
+++ lib/collection_cache_key.rb
@@ -1,6 +1,8 @@
 require 'active_record'
 require 'collection_cache_key/active_record'
 require 'collection_cache_key/cache_key'
+require 'collection_cache_key/relation'
 require 'collection_cache_key/version'
 
 ActiveRecord::Base.extend CollectionCacheKey::CacheKey
+ActiveRecord::Relation.include CollectionCacheKey::Relation
